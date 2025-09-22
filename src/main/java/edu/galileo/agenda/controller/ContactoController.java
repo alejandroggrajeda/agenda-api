57ffd2b011a722a8e09af2583d629a3f407839b2
alejandroggrajeda <alejandroggrajeda@gmail.com>
2025-09-21 18:53:23 -0600

    first commit

--- src/main/java/edu/galileo/agenda/controller/ContactoController.java
+++ src/main/java/edu/galileo/agenda/controller/ContactoController.java
@@ -1,9 +1,7 @@
 package edu.galileo.agenda.controller;
 
-
 import edu.galileo.agenda.model.Contacto;
 import edu.galileo.agenda.service.ContactoService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,18 +9,24 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/contacto")
 public class ContactoController {
-    private ContactoService contactoService;
-    ContactoController(ContactoService contactoService){
+    private final ContactoService contactoService;
+
+    ContactoController(ContactoService contactoService) {
         this.contactoService = contactoService;
     }
 
     @GetMapping
-    public List<Contacto> getAll(){
+    public String getMiNombre() {
+        return "Alejandro Garcia - 18011502";
+    }
+
+    @GetMapping("/lista")
+    public List<Contacto> getAll() {
         return this.contactoService.List();
     }
 
     @PostMapping
-    public Contacto add(@RequestBody(required = true) Contacto contacto){
+    public Contacto add(@RequestBody(required = true) Contacto contacto) {
         return this.contactoService.add(contacto);
     }
 }
